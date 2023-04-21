0a32a2ed224114511d52cedace25f47b1834a471
Enner Escobedo <enner.escobedo@nagarro.com>
2023-04-20 23:51:12 -0600

    Agrega callback y promises ejemplos

--- sample2.js
+++ sample2.js
@@ -7,15 +7,38 @@ let stocks = {
 
 // Function 1
 let order = (fruit_name, call_production) => {
-    console.log("Order placed. Please call production");
-
-    call_production();
+    setTimeout(function () {
+        console.log(`${stocks.Fruits[fruit_name]} was selected`);
+        call_production();
+    }, 2000);
 };
 
 // Function 2
 let production = () => {
-    console.log("Production has started");
+
+    setTimeout(()=>{
+        console.log("production has started")
+        setTimeout(()=>{
+            console.log("The fruit has been chopped")
+            setTimeout(()=>{
+                console.log(`${stocks.liquid[0]} and ${stocks.liquid[1]} Added`)
+                setTimeout(()=>{
+                    console.log("start the machine")
+                    setTimeout(()=>{
+                        console.log(`Ice cream placed on ${stocks.holder[1]}`)
+                        setTimeout(()=>{
+                            console.log(`${stocks.toppings[0]} as toppings`)
+                            setTimeout(()=>{
+                                console.log("serve Ice cream")
+                            },2000)
+                        },3000)
+                    },2000)
+                },1000)
+            },1000)
+        },2000)
+    },0000)
+
 };
 
 // name of our second function, Trigger
-order("", production);
+order(0, production);
